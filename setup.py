ccfc41c3c1bcee4429d0a712631f63e21b0a44c4
Denys Rastiegaiev <denys.rastiegaiev@wpengine.com>
2025-01-10 14:23:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='docker-resource-limiter',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(include=['docker_resource_limiter']),
     install_requires=['docker'],
     entry_points={
         'console_scripts': [
